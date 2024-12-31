774926fd7ff28e4906aa93afdd34d2345bb1cdaa
nekoya10 <kashikoma12@gmail.com>
2024-12-31 12:37:55 +0000

    lesson21

---
+++ lesson21/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/<string:name>")
+def index(name:str=""):
+    return f"<h1>Hello,{name}您好!</h1>"
+
+@app.route("/hello")
+def hello():
+    return "Hi,MAM!"
